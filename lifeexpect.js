7733b26c75134b9e95f10bbbe522230cd4a5469a
Emily Kapor <emily@emilyaviva.com>
2015-07-02 01:51:19 -0400

    added lifeexpect.js with bonus groupBy

--- lifeexpect.js
+++ lifeexpect.js
@@ -1,8 +1,23 @@
+var ancestry = JSON.parse(require('./ancestry.js'));
+
 function average(arr) {
   function plus(a, b) {
     return a + b;
   }
   return arr.reduce(plus) / arr.length;
+}
+
+var everyCentury = {};
+ancestry.forEach(function(p) {
+  var theirCentury = Math.ceil(p.died / 100);
+  if (!(theirCentury in everyCentury)) {
+    everyCentury[theirCentury] = [];
+  }
+  everyCentury[theirCentury].push(p.died - p.born);
+});
+
+for (century in everyCentury) {
+  console.log(century + ':', average(everyCentury[century]));
 }
 
 // → 16: 43.5
@@ -11,3 +26,23 @@ function average(arr) {
 //   19: 54.8
 //   20: 84.7
 //   21: 94
+
+function groupBy(arr, callback) {
+  return arr.map(callback).reduce(function(grp, x, i) {
+    if (!(x in grp)) grp[x] = [arr[i]];
+    else grp[x].push(arr[i]);
+    return grp;
+  }, {});
+};
+
+// bonus: abstract grouping into its own operation
+
+var groups = groupBy(ancestry, function(p) {
+  return Math.ceil(p.died / 100);
+});
+
+for (century in groups) {
+  console.log(century + ':', average(groups[century].map(function(p) {
+    return p.died - p.born;
+  })));
+}
